c3b7e721858145115f7cd8eb380a5c56f0d52143
Samik Pradhan <samikpradhan@gmail.com>
2023-08-17 01:01:16 -0700

    updated boilerplate and constants

--- src/main.py
+++ src/main.py
@@ -1,23 +1,33 @@
 import pygame
+from pygame.locals import *
 from packages.consts import *
 
-# initialize engine
+# pygame setup
 pygame.init()
 
 screen = pygame.display.set_mode(RESOLUTION)
 pygame.display.set_caption("Snake")
 
-# set max FPS
 clock = pygame.time.Clock()
 
+# game loop
 running = True
 while running:
+    # event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
 
-    # clear the screen
-    screen.fill(BLACK)
+    # clear last frame
+    screen.fill("black")
+
+    # RENDER GAME HERE
+
+    # flip() display to put your work on screen
+    pygame.display.flip()
 
     # max updates per second
     clock.tick(FPS)
