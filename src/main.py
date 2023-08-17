750799f60abfd4e62b3f2858e95af1e32d63b865
Samik Pradhan <samikpradhan@gmail.com>
2023-08-17 01:58:32 -0700

    drawing board

--- src/main.py
+++ src/main.py
@@ -9,6 +9,10 @@ screen = pygame.display.set_mode(RESOLUTION)
 pygame.display.set_caption("Snake")
 
 clock = pygame.time.Clock()
+dt = 0  # initialize var for delta time
+
+# initialize game objects
+board = pygame.Rect(BOARD_POS, BOARD_SIZE)
 
 # game loop
 running = True
@@ -22,14 +26,36 @@ while running:
                 running = False
 
     # clear last frame
-    screen.fill("black")
+    screen.fill("lightgreen")
 
     # RENDER GAME HERE
+
+    # board
+    pygame.draw.rect(screen, "green", board, border_radius=5)
+    pygame.draw.rect(screen, "black", board, 5, 5)
+    # horizontal gridlines
+    for x in range(
+        BOARD_POS[0] + BOARD_SIZE[0] // 20,
+        BOARD_POS[0] + BOARD_SIZE[0],
+        BOARD_SIZE[0] // 20,
+    ):
+        pygame.draw.line(
+            screen, "black", (x, BOARD_POS[1]), (x, BOARD_POS[1] + BOARD_SIZE[1] - 2), 3
+        )
+    # vertical gridlines
+    for y in range(
+        BOARD_POS[1] + BOARD_SIZE[1] // 20,
+        BOARD_POS[1] + BOARD_SIZE[1],
+        BOARD_SIZE[1] // 20,
+    ):
+        pygame.draw.line(
+            screen, "black", (BOARD_POS[0], y), (BOARD_POS[0] + BOARD_SIZE[0] - 2, y), 3
+        )
 
     # flip() display to put your work on screen
     pygame.display.flip()
 
     # max updates per second
-    clock.tick(FPS)
+    dt = clock.tick(FPS) / 1000  # dt in seconds since last frame
 
 pygame.quit()
