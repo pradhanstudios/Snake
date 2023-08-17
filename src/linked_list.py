bd03d5a4ffbcdfbf96e6598a79bfa283d2441cf9
Samik Pradhan <samikpradhan@gmail.com>
2023-08-17 16:20:36 -0700

    updated linked_list.py

--- src/linked_list.py
+++ src/linked_list.py
@@ -2,11 +2,16 @@ from packages.consts import *
 
 
 class Node:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int) -> None:
         self.coord = (x, y)
         self.next = None
 
 
-class Snake:
-    def __init__(self):
+class LinkedList:
+    def __init__(self) -> None:
         self.head = Node(NUM_COLS // 2, NUM_ROWS // 2)
+
+
+class Snake:
+    def __init__(self) -> None:
+        self.body = LinkedList()
