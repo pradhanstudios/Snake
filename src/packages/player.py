e3591819885967f609b11bf8a5d76842b9c9f30d
Samik Pradhan <samikpradhan@gmail.com>
2023-08-17 18:30:54 -0700

    fixed imports

--- src/packages/player.py
+++ src/packages/player.py
@@ -1,13 +1,14 @@
 from pygame.rect import Rect
-from . import linked_list as ll
+from linked_list import LinkedList
+
 
 class Player:
     def __init__(self, x, y, w, h, color: tuple[int]):
-        self.body = ll.LinkedList()
+        self.body = LinkedList()
         self.body.insert((Rect(x, y, w, h), color))
         self.cur_player = self.body.head
 
-    def __str__(self): # for testing
+    def __str__(self):  # for testing
         x, y, w, h = self.cur_player.data[0]
         color = self.cur_player.data[1]
         return "pos: %s, %s; size: %s %s; color: %s" % (x, y, w, h, color)
