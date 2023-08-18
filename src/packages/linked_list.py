c32a0e1ea797fb3c8fe996d4b578e579951f5b58
Ritik <flipit001@gmail.com>
2023-08-17 17:51:30 -0700

    added a next function

--- src/packages/linked_list.py
+++ src/packages/linked_list.py
@@ -13,18 +13,15 @@ class LinkedList:
     def insert(self, data: p.Player):
         new_node = Node(data)
 
-        # 4. If the Linked List is empty, then make the
         # new node as head
         if self.head is None:
             self.head = new_node
             return
 
-        # 5. Else traverse till the last node
         last = self.head
         while (last.next):
             last = last.next
 
-        # 6. Change the next of last node
         last.next = new_node
 
     def printll(self):
@@ -32,3 +29,7 @@ class LinkedList:
         while temp:
             print(temp.data)
             temp = temp.next
+
+    def next(self):
+        self.head = self.head.next
+
