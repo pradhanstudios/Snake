ee277806726163a9e877697d8c93594d5ff2ecc2
Ritik <flipit001@gmail.com>
2023-08-17 17:04:49 -0700

    made a linked list

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,17 +1,47 @@
 from packages.consts import *
+# from pygame.rect import Rect
 
 
 class Node:
-    def __init__(self, x: int, y: int) -> None:
-        self.coord = (x, y)
-        self.next = None
+    def __init__(self, xy: tuple[int], next=None) -> None:
+        self.coord = xy
+        self.next = next
 
 
 class LinkedList:
-    def __init__(self) -> None:
-        self.head = Node(NUM_COLS // 2, NUM_ROWS // 2)
+    def __init__(self, head=None) -> None:
+        self.head = head
+
+    def insert(self, data):
+        new_node = Node(data)
+
+        # 4. If the Linked List is empty, then make the
+        # new node as head
+        if self.head is None:
+            self.head = new_node
+            return
+
+        # 5. Else traverse till the last node
+        last = self.head
+        while (last.next):
+            last = last.next
+
+        # 6. Change the next of last node
+        last.next = new_node
+
+    def printll(self):
+        temp = self.head
+        while temp:
+            print(temp.coord)
+            temp = temp.next
+
+    
 
 
-class Snake:
-    def __init__(self) -> None:
-        self.body = LinkedList()
+# class Snake:
+#     def __init__(self, x, y, w, h) -> None:
+#         self.body = LinkedList(Rect(x, y, w, h))
+
+#     def draw_body(self):
+        
+    
