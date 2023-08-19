f850560a753aef6d81c216f11f834969c132d188
Ritik <flipit001@gmail.com>
2023-08-18 18:20:56 -0700

    added snake.py

---
+++ src/packages/Snake.py
@@ -0,0 +1,17 @@
+from pygame.rect import Rect
+from packages.linked_list import LinkedList
+
+
+class Player:
+    def __init__(self, xy: tuple):
+        self.body = LinkedList()
+        self.body.insert(xy)
+        self.cur_player = self.body.head
+
+    def __str__(self):  # for testing
+        x, y, w, h = self.cur_player.data[0]
+        color = self.cur_player.data[1]
+        return "pos: %s, %s; size: %s %s; color: %s" % (x, y, w, h, color)
+    
+    def add_cell(self, xy: tuple):
+        self.body.insert(xy)
